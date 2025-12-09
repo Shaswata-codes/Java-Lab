e2c4e775ec42b3fd0466d38d3c1bb8f196372399
Shaswata Sarkar <shaswatasarkar05@gmail.com>
2025-12-09 13:10:35 +0530

    Day1

---
+++ Day 1/PalindromeCheck.java
@@ -0,0 +1,16 @@
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        int num = 121;    // Mention the number here
+        int temp = num, rev = 0;
+
+        while(temp > 0) {
+            rev = rev * 10 + (temp % 10);
+            temp /= 10;
+        }
+
+        if(num == rev)
+            System.out.println(num + " is a Palindrome");
+        else
+            System.out.println(num + " is Not a Palindrome");
+    }
+}
